01c5aa05b4669891c77c56d0b31f678f8fb3ca5c
abc100m <jianlinlong@gmail.com>
2016-03-27 19:08:57 +0800

    change code style, :)
    add deallocate_String_vector to fix memory leak

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -DTHREADED -g -O2 -D_GNU_SOURCE -o zk_test zk_test.cpp ./.libs/libzookeeper_mt.dll.a -lpthread -L/usr/local/lib -I/usr/local/include -I./ -std=c++0x
+g++ -DTHREADED -g -O2 -D_GNU_SOURCE -o zk_test zk_test.cpp -lzookeeper_mt -lpthread -L/usr/local/lib -I/usr/local/include -I./ -std=c++11
